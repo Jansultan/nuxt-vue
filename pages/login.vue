180b23b664feea50e6a15d27df1c73dfca4af015
Zhansultan <=jansultan_141096@mail.ru>
2020-11-06 23:09:28 +0600

    Initial commit

---
+++ pages/login.vue
@@ -0,0 +1,38 @@
+<template>
+    <section>
+        <div v-if="$route.query.message" class="alert alert-danger mb-3">
+            Need login first
+        </div>
+        <h1>Login page</h1>
+        <form @submit.prevent="onSubmit">
+            <div class="form-group">
+                <input type="text" class="form-control">
+            </div>
+            <p>
+                <nuxt-link to="/">To home page</nuxt-link>
+            </p>
+
+            <button class="btn btn-primary">Login</button>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+    layout:'empty',
+    methods:{
+        onSubmit(){
+            this.$store.dispatch('login')
+            this.$router.push('/')
+        }
+    }
+}
+</script>
+
+<style scoped>
+section{
+    width: 500px;
+    margin: 0 auto;
+    padding-top: 30px;
+}
+</style>
